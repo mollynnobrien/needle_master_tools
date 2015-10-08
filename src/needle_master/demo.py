c114c7570bc44103e6cdd451e5ac97bec50a8ab4
Chris Paxton <paxton.chris@live.com>
2015-10-08 11:43:35 -0400

    initial commit

---
+++ src/needle_master/demo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 08 11:31:19 2015
+
+@author: Chris
+"""
+
+import numpy as np
+
+'''
+Stores data for a single performance of a task.
+You can pull this data out as a Numpy array.
+'''
+class Demo:
+    
+    def __init__(self):
+        pass
+    
+    '''
+    Load demonstration from a file
+    '''
+    def Load(self,handle):
+        pass
---
+++ src/needle_master/environment.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 08 11:30:52 2015
+
+@author: Chris Paxton
+"""
+
+import numpy as np
+
+class Environment:
+    
+    def __init__(self):
+        pass
+    
+    '''
+    Load an environment file.
+    '''
+    def Load(self,handle):
+        pass
+    
+class Gate:
+    
+    def __init__(self):
+        pass
+    
+    '''
+    Load Gate from file at the current position.
+    '''
+    def Load(self,handle):
+        pass
