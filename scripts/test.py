7a7d08a6de535dfe75ce5c798923b93fd88d8747
Christopher Paxton <cpaxton3@jhu.edu>
2015-10-08 15:28:49 -0400

    reading and drawing environments

--- scripts/test.py
+++ scripts/test.py
@@ -3,12 +3,19 @@
 import needle_master as nm
 import os
 
+import matplotlib.pyplot as plt
+
 files = os.listdir('.')
 
 for file in files:
     if file[:5] == 'trial':
         # process as a trial
         demo = nm.Demo(file)
-    elif file[:11] == 'environment':
-        # process as an environment
-        env = nm.Environment(file)
+for i in range(1,9):
+    filename = "environment_%d.txt"%(i)
+
+    # process as an environment
+    env = nm.Environment(filename)
+    plt.subplot(2,4,i)
+    env.Draw()
+plt.show()
--- src/needle_master/__init__.py
+++ src/needle_master/__init__.py
@@ -1,3 +1,4 @@
 from demo import *
 from environment import *
 from file import *
+from utils import *
