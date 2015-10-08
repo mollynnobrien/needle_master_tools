0c2ec2d94d65c4dcc6074f9dd223096bd0556810
Christopher Paxton <cpaxton3@jhu.edu>
2015-10-08 15:54:28 -0400

    making sure utilities are included

--- src/needle_master/demo.py
+++ src/needle_master/demo.py
@@ -6,6 +6,7 @@ Created on Thu Oct 08 11:31:19 2015
 """
 
 import numpy as np
+import needle_master as nm
 
 '''
 Stores data for a single performance of a task.
@@ -16,11 +17,14 @@ class Demo:
     def __init__(self,filename=None):
         if not filename is None:
             handle = file(filename,'r')
+            (env,time) = nm.ParseDemoName(filename)
             self.Load(handle)
+            self.env = env
 
     
     '''
     Load demonstration from a file
     '''
     def Load(self,handle):
-        pass
+        data = handle.readline()
+        print data
