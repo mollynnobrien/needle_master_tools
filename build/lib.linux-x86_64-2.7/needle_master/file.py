31489a568f5424a001268093371040f5318fc8b9
Molly O'Brien <molly@jhu.edu>
2018-12-05 14:34:39 -0500

    draw needle from demonstrations

---
+++ build/lib.linux-x86_64-2.7/needle_master/file.py
@@ -0,0 +1,12 @@
+
+'''
+file utilities
+'''
+
+def ParseEnvironmentName(filename):
+    toks = filename.split('/')[-1].split('.')[0].split('_')
+    return toks[1]
+
+def ParseDemoName(filename):
+    toks = filename.split('/')[-1].split('.')[0].split('_')
+    return (int(toks[1]),toks[2])
