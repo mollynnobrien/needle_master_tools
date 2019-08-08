c4171e95fe2b2f6a851a39c3d95dcb7d674cb019
Yotam Barnoy <yotambarnoy@gmail.com>
2019-08-08 15:38:05 -0400

    Change some argument defaults

--- TD3/main_image.py
+++ TD3/main_image.py
@@ -280,12 +280,13 @@ if __name__ == "__main__":
         help='How often (time steps) we evaluate')
     parser.add_argument("--pid_freq", default=1e4, type=int,
         help='How often we get back to pure random action')
-    parser.add_argument("--max_timesteps", default=5e6, type=float,
+    parser.add_argument("--max_timesteps", default=5e7, type=float,
         help='Max time steps to run environment for')
     parser.add_argument("--learning_start", default=0, type=int,
         help='Timesteps before learning')
     parser.add_argument("--save_models", action= "store",
         help='Whether or not models are saved')
+
     #--- Exploration Noise
     parser.add_argument("--no-ou-noise", default=False, action='store_true',
         help='Use OU Noise process for noise instead of epsilon greedy')
@@ -294,6 +295,7 @@ if __name__ == "__main__":
     parser.add_argument("--epsilon_greedy", default=0.3, type=float,
         help='Starting percentage of choosing random noise')
     #---
+
     #--- Batch size is VERY important ---
     parser.add_argument("--batch-size", default=1024, type=int,
         help='Batch size for both actor and critic')
@@ -301,15 +303,10 @@ if __name__ == "__main__":
     parser.add_argument("--discount", default=0.99, type=float,
         help='Discount factor (0.99 is good)')
 
-    parser.add_argument("--tau", default=0.005, type=float,
-        help='Target critic network update rate')
-    parser.add_argument("--actor-tau", default=0.02, type=float,
-        help='Target actor network update rate')
-
-    parser.add_argument("--policy_noise", default=0.2, type=float,
+    parser.add_argument("--policy_noise", default=0.04, type=float, # was 0.2
         help='TD3 Smoothing noise added to target policy during critic update')
-    parser.add_argument("--noise_clip", default=0.5, type=float,
-        help='TD3 Range to clip target policy noise')
+    parser.add_argument("--noise_clip", default=0.1, type=float,
+        help='TD3 Range to clip target policy noise') # was 0.5
 
     parser.add_argument("--max_size", default=1e6, type=int,
         help='Size of replay buffer (bigger is better)')
@@ -321,20 +318,31 @@ if __name__ == "__main__":
         help="Profile the program for performance")
     parser.add_argument("--mode", default = 'state',
         help="Choose image or state, options are rgb_array and state")
-    parser.add_argument("--buffer", default = 'priority',
+    parser.add_argument("--buffer", default = 'simple', # 'priority'
         help="Choose type of buffer, options are simple and priority")
     parser.add_argument("--random_needle", default = False, action='store_true',
         help="Choose whether the needle should be random at each iteration")
     parser.add_argument("--batchnorm", default = False,
         action='store_true', help="Choose whether to use batchnorm")
 
-    parser.add_argument("--policy_freq", default=3, type=int,
+    parser.add_argument("--policy_freq", default=2, type=int,
         help='Frequency of TD3 delayed actor policy updates')
 
-    parser.add_argument("--lr", default=1e-4, type=float,
-        help="Learning rate for optimizer")
-    parser.add_argument("--actor-lr", default=1e-3, type=float,
+    #--- Tau: percent copied to target
+    parser.add_argument("--tau", default=0.001, type=float,
+        help='Target critic network update rate')
+    parser.add_argument("--actor-tau", default=0.001, type=float,
+        help='Target actor network update rate')
+    #---
+
+    #--- Learning rates
+    parser.add_argument("--lr", default=1e-3, type=float,
+        help="Learning rate for critic optimizer")
+    parser.add_argument("--lr2", default=1e-3, type=float,
+        help="Learning rate for second critic optimizer")
+    parser.add_argument("--actor-lr", default=1e-5, type=float,
         help="Learning rate for actor optimizer")
+    #---
 
     parser.add_argument("filename", help='File for environment')
     parser.add_argument("policy_name", default="TD3", type=str)
